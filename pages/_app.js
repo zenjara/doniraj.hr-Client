cfcf3e24c8f43efbaf84cabcb3b6cd1bd61bf945
Kristijan Korać <korac.kristijan@gmail.com>
2021-03-11 22:47:23 +0100

    Add theme-ui package; Create ui folder with theme; Export theme and consume it with ThemeProvider.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css';
 
+import { ThemeProvider } from 'theme-ui';
+
+import { theme } from '../ui';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
